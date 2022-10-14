12914ed5cc285b8a056a2a0c301d9473bdfaeea5
Randy <linyejoe2@gmail.com>
2022-10-14 17:33:28 +0800

    modified HelloWorld/main.go

--- HelloWorld/main.go
+++ HelloWorld/main.go
@@ -1,5 +1,8 @@
-package main
+package helloworld
+
+import "fmt"
 
 func main() {
-	print("go")
+	fmt.Println("Hello World!")
+	fmt.Print("This is a tutorial of GitHub Deploy keys by linyejoe2!")
 }
